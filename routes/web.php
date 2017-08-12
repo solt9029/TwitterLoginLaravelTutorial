aeb5a3ea27962d6f6168fbf8d11771b88c51a877
Kenshi Shiode <atemu1212@yahoo.co.jp>
2017-08-12 21:57:40 +0900

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', array('as' => 'login', 'uses' => function(){
+    return view('welcome');
+}));
+
+Route::get('auth/twitter', 'Auth\SocialAuthController@redirectToProvider');
+Route::get('auth/twitter/callback', 'Auth\SocialAuthController@handleProviderCallback');
+Route::get('auth/twitter/logout', 'Auth\SocialAuthController@logout');
+
+Route::get('home', array('as' => 'home', 'uses' => function(){
+    if(!Auth::check()){
+        return redirect()->route("login");
+    }
+    return view('home');
+}));
